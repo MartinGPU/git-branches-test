03be36e25e149cd96650a50c0b57ecaf78b96d56
Marat <maratussr@gmail.com>
2023-01-14 20:53:24 +0400

    init commit
    added hover and timing
    
    added selecting product type
    
    added searching product type and timing

---
+++ src/test/java/com/marat/test/GitTest.java
@@ -0,0 +1,30 @@
+package com.marat.test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class GitTest {
+
+    @BeforeAll
+    public static void beforeAll() {
+        Configuration.browserSize = "1366x768";
+    }
+
+    @Test
+    public void testForGit() {
+        Selenide.open("https://www.avito.ru/");
+        sleep(4000);
+        $(".top-rubricator-rubricatorButton-SoKyQ").click();
+        sleep(2000);
+        $("[data-marker = 'top-rubricator/root-category-26195']").hover();
+        $(byText("Рации")).click();
+        $(".input-input-Zpzc1").setValue("Motorolla");
+        sleep(2000);
+    }
+}
